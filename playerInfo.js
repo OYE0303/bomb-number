62f68f2b9568c0b0a904205a26d7124918ef949e
OYE0303 <78901921+OYE0303@users.noreply.github.com>
2021-08-09 00:42:09 +0800

    Add files via upload

---
+++ playerInfo.js
@@ -0,0 +1,9 @@
+export class PLAYER__INFO {
+  constructor(countryName, order, toolCounts, countryData, position) {
+    this.countryName = countryName;
+    this.order = order;
+    this.toolCounts = toolCounts;
+    this.countryData = countryData;
+    this.position = position;
+  }
+}
